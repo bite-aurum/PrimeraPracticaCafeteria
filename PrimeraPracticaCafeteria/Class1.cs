465ef1885235ef29e4fe55c3b1a34a9a06fcd216
bite-aurum <saidmiguelmezalopez@gmail.com>
2026-05-19 09:50:44 -0600

    Agregar archivos de proyecto.

---
+++ PrimeraPracticaCafeteria/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PracticaCafeteria
+{
+	class Bebida
+	{
+		private string nombre;
+		private string tamaño;
+		private double precio;
+
+		public Bebida(string nombre, string tamaño, double precio)
+		{
+			this.nombre = nombre;
+			this.tamaño = tamaño;
+			this.precio = precio;
+		}
+
+		
+		public void Preparar()
+		{
+			Console.WriteLine("Preparando un " + nombre + " tamaño " + tamaño + "...");
+		}
+
+		public void AplicarDescuento(double porcentaje)
+		{
+			double descuento = precio * (porcentaje / 100);
+			precio = precio - descuento;
+		}
+
+		public void MostrarDescripcion()
+		{
+			Console.WriteLine("Esto es un " + nombre + " de tamaño " + tamaño +" con un costo de $" + precio);
+		}
+	}
+
+}
---
+++ PrimeraPracticaCafeteria/Program.cs
@@ -0,0 +1,48 @@
+﻿using PracticaCafeteria;
+
+namespace PrimeraPracticaCafeteria
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Bebida[] bebidas = new Bebida[3];
+
+			string nombre, tamaño;
+			double precio;
+
+			for (int i = 0; i < 3; i++)
+			{
+				Console.WriteLine($"\nIngrese nombre de la bebida {i}:");
+				nombre = Console.ReadLine();
+
+				Console.WriteLine("Ingrese tamaño:");
+				tamaño = Console.ReadLine();
+
+				Console.WriteLine("Ingrese precio:");
+				precio = Convert.ToDouble(Console.ReadLine());
+
+				bebidas[i] = new Bebida(nombre, tamaño, precio);
+			}
+
+			bebidas[0].AplicarDescuento(10);
+			bebidas[1].AplicarDescuento(10);
+			bebidas[2].AplicarDescuento(10);
+
+			Console.WriteLine("\nPreparación\n");
+
+			for (int i = 0; i < 3; i++)
+			{
+				bebidas[i].Preparar();
+			}
+
+			Console.WriteLine("\nDescripción\n");
+
+			for (int i = 0; i < 3; i++)
+			{
+				bebidas[i].MostrarDescripcion();
+			}
+		}
+	}
+}
